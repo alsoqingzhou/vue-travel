a9c9e547bd6727f8dbb5aa1405a17f1ea61b6820
unknown <alsoqingzhou@gmail.com>
2020-07-22 10:37:40 +0800

    simplify code

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -21,10 +21,11 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+    @import '~styles/varibles.styl'
     .header
         display flex
         line-height .86rem
-        background #00bcd4
+        background $bgColor
         color #ffffff
         .header-left
             width .64rem
