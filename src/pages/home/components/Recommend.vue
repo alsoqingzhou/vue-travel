2f9b6a659f255b199911a5b23c46ffeeaf8a808c
unknown <alsoqingzhou@gmail.com>
2020-07-24 22:42:36 +0800

    add recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,78 @@
+<template>
+    <div>
+        <div class="recommend-title">热销推荐</div>
+        <ul>
+            <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+                <img class="item-img"
+                    :src="item.imgUrl" />
+                <div class="item-info">
+                    <p class="item-title">{{item.title}}</p>
+                    <p class="item-desc">{{item.desc}}</p>
+                    <button class="item-button">查看详情</button>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1507/cc/19733fc0135062788140cbb48ae606a7.water.jpg_200x200_d9ebe2fd.jpg',
+        title: '北京野生动物园',
+        desc: '敢与森林之王近距离接触吗'
+      }, {
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1507/cc/19733fc0135062788140cbb48ae606a7.water.jpg_200x200_d9ebe2fd.jpg',
+        title: '北京野生动物园',
+        desc: '敢与森林之王近距离接触吗'
+      }, {
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1507/cc/19733fc0135062788140cbb48ae606a7.water.jpg_200x200_d9ebe2fd.jpg',
+        title: '北京野生动物园',
+        desc: '敢与森林之王近距离接触吗'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+    .recommend-title
+        margin-top: .2rem
+        line-height: .6rem
+        background: #eeeeee
+        text-indent: .2rem
+    .item
+        overflow: hidden
+        display: flex
+        height: 1.9rem
+        .item-img
+            width: 1.7rem
+            height: 1.7rem
+            padding: .1rem
+        .item-info
+            flex: 5
+            padding: .1rem
+            min-width: 0
+            .item-title
+                line-height: .54rem
+                font-size: .32rem
+                ellipsis()
+            .item-desc
+                line-height: .4rem
+                color: #cccccc
+                ellipsis()
+            .item-button
+                line-height: .44rem
+                margin-top: .2rem
+                background: #ff9300
+                color: #ffffff
+                padding: 0 .2rem
+                border-radius: .06rem
+</style>
